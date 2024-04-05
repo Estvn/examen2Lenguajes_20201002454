3699b4966224255e2fe838edba8336d10dee1d80
Estvn <ir51370@gmail.com>
2024-04-05 16:53:33 -0600

    subiendo carpetas vacias

---
+++ src/main/java/hn/unah/lenguajes1900/data/examen2lenguajes/Examen2lenguajesApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes1900.data.examen2lenguajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2lenguajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2lenguajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/unah/lenguajes1900/data/examen2lenguajes/Examen2lenguajesApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes1900.data.examen2lenguajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2lenguajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
